c193449e87af72e40b4766989e6a80ef39bed474
mr-manson <nikola777jr@gmail.com>
2024-10-15 21:44:36 +0300

    ts literal types

--- app.ts
+++ app.ts
@@ -1,25 +1,5 @@
-const logId = (id: string | number | boolean) => {
-	if (typeof id === "string") {
-		console.log(id);
-	} else if (typeof id === "number") {
-		console.log(id);
-	} else {
-		console.log(id);
-	}
+const fetchWithAuth = (url: string, method: 'post' | 'get') => {
+
 };
 
-const logError = (err: string | string[]) => {
-	if (Array.isArray(err)) {
-		console.log(err);
-	} else {
-		console.log(err);
-	}
-};
-
-const logObj = (obj: {a: number} | {b: number}) => {
-	if ('a' in obj) {
-		console.log(obj.a);
-	} else {
-		console.log(obj.b);
-	}
-};
+fetchWithAuth('http://localhost:8080', 'post');
