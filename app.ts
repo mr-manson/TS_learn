a75e0e1b5ca92aedf3cd7fc1d3aba135ec79ca96
mr-manson <nikola777jr@gmail.com>
2024-10-17 13:24:12 +0300

    ts interface dictionary

--- app.ts
+++ app.ts
@@ -2,9 +2,15 @@ interface User {
 	name: string,
 	age: number,
 	skills: string[],
+
+	log: (id: number) => string,
 }
 
 interface Role {
+	id: number,
+}
+
+type Role2 = {
 	id: number,
 }
 
@@ -16,4 +22,13 @@ let user: UserWithRole = {
 	age: 40,
 	skills: ['1', '2', '3'],
 	id: 1,
+
+	log(id) {
+		return '';
+	}
+}
+
+//интерфейс-словарь, сегодня не используется т.к. есть Records
+interface UserDic{
+	[index: number]: User
 }
