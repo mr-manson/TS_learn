b0da3c8a7b7a4fd4d573b3050e8c41d7fa0ed4e6
mr-manson <nikola777jr@gmail.com>
2024-10-20 21:46:50 +0300

    ts type guard +

--- app.ts
+++ app.ts
@@ -27,3 +27,19 @@ function logId(id: string | number) {
 function isString (x: string | number) : x is string {
 	return typeof x === 'string';
 }
+
+function isAdmin (user: User | Admin): user is Admin {
+	return 'role' in user;
+}
+
+function isAdmin2 (user: User | Admin): user is Admin {
+	return (user as Admin).role !== undefined;
+}
+
+function setRoleZero (user: User | Admin) {
+	if (isAdmin(user)) {
+		user.role = 0;
+	} else {
+		throw new Error('User is not admin');
+	}
+}
