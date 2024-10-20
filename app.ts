3c37aa1be74c8944491b7fc870c14d16b4a7eb05
mr-manson <nikola777jr@gmail.com>
2024-10-20 20:25:48 +0300

    ts type cust

--- app.ts
+++ app.ts
@@ -1,19 +1,19 @@
+let a: number = 5;
+let b: string = a.toString();
+
+let c: string = 'asdfg';
+let d: number = parseInt(c);
+
+let e: string = new String(a).valueOf();
+
 interface User {
-	name: string
+	name: string;
+	email: string;
+	login: string;
 }
 
-const getUser = () => {
-	if(Math.random() > 0.5 ){
-		return null; //без null будет undefined
-	} else {
-		return {
-			name: 'Вася'
-		}
-	}
-}
-
-const user = getUser();
-if(user){
-	const n = user.name
-}
-
+const user = {
+	name: 'Вася',
+	email: 'vasia@gmail.com',
+	login: 'vasia',
+} as User; // или : User
