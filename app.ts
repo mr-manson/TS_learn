df31b24e8cf7f34ae9451b745de11c10fe29f4e5
mr-manson <nikola777jr@gmail.com>
2024-10-20 20:13:00 +0300

    ts null

--- app.ts
+++ app.ts
@@ -1,40 +1,19 @@
-function generateError(message: string): never { // функция никогда ничего не возвращает
-	throw new Error(message);
+interface User {
+	name: string
 }
 
-function dumpError(): never {
-	while (true) {
+const getUser = () => {
+	if(Math.random() > 0.5 ){
+		return null; //без null будет undefined
+	} else {
+		return {
+			name: 'Вася'
+		}
 	}
 }
 
-function rec(): never {
-	return rec();
+const user = getUser();
+if(user){
+	const n = user.name
 }
 
-type paymentAction = 'refund' | 'checkout' | 'reject';
-
-function processAction(action: paymentAction) {
-	switch (action) {
-		case 'refund':
-			// ....
-			break;
-		case 'checkout':
-			// ....
-			break;
-		case 'reject':
-			// ....
-			break;
-		default:
-			const _: never = action; // проверка если не все экшены обработаны
-			throw new Error('Unknown payment action');
-	}
-}
-
-function isString(x: string | number): boolean {
-	if (typeof x === 'string') {
-		return true;
-	} else if (typeof x === 'number') {
-		return false;
-	}
-	generateError('asasas');
-}
