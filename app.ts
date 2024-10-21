8af2d716a916a697434eb6647c73e907e0862734
mr-manson <nikola777jr@gmail.com>
2024-10-21 22:16:00 +0300

    ts class

--- app.ts
+++ app.ts
@@ -1,45 +1,7 @@
-interface User {
+class User {
 	name: string;
-	email: string;
-	login: string;
-}
 
-const user = {
-	name: 'Вася',
-	email: 'vasia@gmail.com',
-	login: 'vasia',
-} as User; // или : User
-
-interface Admin {
-	name: string;
-	role: number
-}
-
-function logId(id: string | number) {
-	if (isString(id)) {
-		console.log(id);
-	} else {
-		console.log(id);
-	}
-}
-
-// функция type guard
-function isString (x: string | number) : x is string {
-	return typeof x === 'string';
-}
-
-function isAdmin (user: User | Admin): user is Admin {
-	return 'role' in user;
-}
-
-function isAdmin2 (user: User | Admin): user is Admin {
-	return (user as Admin).role !== undefined;
-}
-
-function setRoleZero (user: User | Admin) {
-	if (isAdmin(user)) {
-		user.role = 0;
-	} else {
-		throw new Error('User is not admin');
+	constructor (name: string) {
+		this.name = name;
 	}
 }
