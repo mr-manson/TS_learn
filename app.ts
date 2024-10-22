2a1e31f025de7e76e9f26b272c416ce751a66c7e
mr-manson <nikola777jr@gmail.com>
2024-10-22 21:04:50 +0300

    ts methods & funk overload

--- app.ts
+++ app.ts
@@ -1,36 +1,23 @@
-enum PaymentStatus {
-	HOLDED,
-	PROCESSED,
-	REVERSED
-}
+class User {
+	skills: string[];
 
-class Payment {
-	id: number;
-	status: PaymentStatus = PaymentStatus.HOLDED;
-	createdAt: Date = new Date();
-	updatedAt: Date;
-
-	constructor(id: number) {
-		this.id = id;
-		//this.createdAt = new Date();
-		//this.status = PaymentStatus.HOLDED; дефолтное значение в свойствах выше
-	}
-
-	getPaymentLifeTime(): number {
-		return new Date().getTime() - this.createdAt.getTime();
-	}
-
-	unholdPayment(): void {
-		if (this.status === PaymentStatus.PROCESSED) {
-			throw new Error('Платеж не может бюыть возвращен');
+	addSkill(skill: string): void;
+	addSkill(skill: string[]): void;
+	addSkill(skillOrSkills: string | string[]): void {
+		if (typeof skillOrSkills === 'string') {
+			this.skills.push(skillOrSkills);
+		} else if (Array.isArray(skillOrSkills)) {
+			this.skills.concat(skillOrSkills);
 		}
-		this.status = PaymentStatus.REVERSED;
-		this.updatedAt = new Date();
 	}
 }
 
-const payment = new Payment(1);
-payment.unholdPayment();
-console.log(payment)
-const time = payment.getPaymentLifeTime();
-console.log(time);
+function run(distance: number): number;
+function run(distance: string): string;
+function run(distance: number | string): number | string {
+	if (typeof distance === 'number') {
+		return distance;
+	} else {
+		return distance;
+	}
+}
