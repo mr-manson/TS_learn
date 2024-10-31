112b5877cf2738c6b92780ca839239c79726556b
mr-manson <nikola777jr@gmail.com>
2024-10-31 21:22:29 +0300

    ts this typification

--- app.ts
+++ app.ts
@@ -1,33 +1,28 @@
-class Payment {
-	private date: Date = new Date();
+class UserBuilder {
+	name: string;
 
-	getDate(this: Payment) { // должны работать с контекстом Payment'а
-		return this.date;
+	setName(name: string): this { //this или UserBuilder
+		this.name = name;
+		return this;
 	}
 
-	getDateArrow = () => { // в стрелочной функции контекст не теряется, он всегда привязан к Payment'y
-		return this.date;
-	}
-
-}
-
-const p = new Payment();
-
-const user = {
-	id: 1,
-	paymentDate: p.getDate.bind(p),
-	paymentDateArrow: p.getDateArrow,
-}
-
-//console.log(p.getDate());
-//console.log(user.paymentDate());
-//console.log(user.paymentDateArrow());
-
-class PaymentPersistent extends Payment {
-	save() {
-		return super.getDate();
-		// super.getDateArrow работать не будет, в super можно получить обычную функцию, но не стрелочную, только если super заменить на this
+	// TYPE GUARD
+	isAdmin(): this is AdminBuilder{
+		return this instanceof AdminBuilder;
 	}
 }
 
-console.log(new PaymentPersistent().save());
+class AdminBuilder extends UserBuilder {
+	roles: string[];
+}
+
+const res = new UserBuilder().setName('Вася');
+const res2 = new AdminBuilder().setName('Петя'); // если в UserBuilder будет тип UserBuilder, то res2 тоже будет UsrBuilder
+
+let user: UserBuilder | AdminBuilder = new UserBuilder();
+
+if (user.isAdmin()){
+	console.log(user);
+} else {
+	console.log(user);
+}
