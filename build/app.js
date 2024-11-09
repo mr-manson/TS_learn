a0f51f2a615ef553504601d5d456e3a6ed8d64d8
mr-manson <nikola777jr@gmail.com>
2024-11-09 21:35:36 +0300

    add ex directory

--- build/app.js
+++ build/app.js
@@ -1,17 +1,17 @@
 "use strict";
-class Vehicle {
+class Resp {
+    constructor(data, error) {
+        if (data) {
+            this.data = data;
+        }
+        if (error) {
+            this.error = error;
+        }
+    }
 }
-function kmToMiles(vehicle) {
-    vehicle.run = vehicle.run / 0.62;
-    return vehicle;
-}
-class LCV extends Vehicle {
-}
-const lcv = new LCV();
-lcv.run = 500;
-console.log(kmToMiles(lcv).run);
-// generic с примитивами
-function logId(id) {
-    console.log(id);
-    return id;
+const res = new Resp('data'); // с одним параметром и без дженериков будет unknown
+class HTTPResp extends Resp {
+    setCode(code) {
+        this.code = code;
+    }
 }
